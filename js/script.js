6bd55da8c4fa6fb77603b8f38f1659d4493c0b41
Mateusz Stachurski <mateusz.stachurski@icloud.com>
2020-07-10 14:20:27 +0200

    Add draw computer movement

--- js/script.js
+++ js/script.js
@@ -1,18 +1,25 @@
-function printMessage(msg){
-	var div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+function printMessage(msg) {
+    var div = document.createElement('div');
+    div.innerHTML = msg;
+    document.getElementById('messages').appendChild(div);
 }
 
-function clearMessages(){
-	document.getElementById('messages').innerHTML = '';
+function clearMessages() {
+    document.getElementById('messages').innerHTML = '';
 }
 
-var playerMove;
-playerMove = 'papier';
-printMessage('Zagrałem ' + playerMove + '! Jeśli Twój ruch to nożyce, to wygrywasz!');
+var computerMove, randomNumber;
+randomNumber = Math.floor(Math.random() * 3 + 1);
+console.log('Wylosowana liczba to: ' + randomNumber);
 
-var randomNubmber;
+if (randomNumber == '1') {
+    computerMove = 'kamień'
+} else if (randomNumber == '2') {
+    computerMove = 'papier'
+} else if (randomNumber == '3') {
+    computerMove = 'nożyce'
+} else {
+    computerMove = 'Nieznany ruch'
+};
 
-randomNumber = Math.floor(Math.random() * 3 + 10);
-printMessage('Wyslosowana liczba to :' + randomNumber);
+printMessage('Mój ruch to: ' + computerMove);
